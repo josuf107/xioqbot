0810a16a55f274b0c5587308aac55050075f0126
Joseph Barratt <jbarratt@indeed.com>
2016-08-10 15:30:38 -0500

    Add test framework #6

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,95 @@
+import Queue hiding (test)
+import Command
+
+import Control.Monad.Writer
+import Control.Monad.State
+import Data.Monoid
+import Data.Time
+
+data TestStep
+    = Wait Seconds
+    | StreamerSay String
+    | UserSay TwitchUser String
+    | BotSay (Maybe String)
+    deriving (Show, Eq, Ord)
+
+type TestSpec = Writer [TestStep] ()
+
+data Test = Test
+    { testSpec :: TestSpec
+    , testDescription :: String
+    } deriving (Show, Eq, Ord)
+
+data TestResult = Pass | Fail String deriving (Show, Eq, Ord)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = mapM_ runTest
+    [ test "Can't enter closed queue" $ do
+        user "a" "!enter"
+        botSay "Sorry the queue is closed so you can't !enter. Use !smash open to open the queue."
+    , test "Can't enter non-indexed user" $ do
+        streamer "!smash open"
+        user "a" "!enter"
+        botSay "a is not in the index. Add yourself with !index nnid miiName."
+    , test "Can enter indexed users into open queue" $ do
+        streamer "!smash open"
+        user "a" "!index annid amiiname"
+        user "a" "!enter"
+        botSay "Added a to the queue! You are at position 1"
+    ]
+
+test desc spec = Test spec desc
+wait :: Seconds -> TestSpec
+wait = tellOne . Wait
+streamer :: String -> TestSpec
+streamer = tellOne . StreamerSay
+user :: String -> String -> TestSpec
+user u = tellOne . UserSay (TwitchUser u)
+botSay :: String -> TestSpec
+botSay = tellOne . BotSay . Just
+botSayNothing :: TestSpec
+botSayNothing = tellOne (BotSay Nothing)
+tellOne = tell . return
+
+runTest :: Test -> IO ()
+runTest t =
+    putStrLn $ case runTestSpec (testSpec t) of
+        Pass -> testDescription t ++ ": Passed!"
+        Fail s -> testDescription t ++ ": Failed! " ++ s
+
+runTestSpec :: TestSpec -> TestResult
+runTestSpec spec = runSteps . snd . runWriter $ spec
+
+runSteps :: [TestStep] -> TestResult
+runSteps steps =
+    case dropWhile (==Pass) . fst . runState (mapM runStep steps) $ testStateStart of
+        [] -> Pass
+        (f:_) -> f
+
+testStateStart = TestState
+    (UTCTime (fromGregorian 2016 8 10) 0)
+    defaultQueue
+    Nothing
+
+data TestState = TestState
+    { testStateTime :: UTCTime
+    , testStateQueue :: Queue
+    , testStateLastMessage :: Maybe String
+    } deriving (Show, Eq, Ord)
+
+runStep :: TestStep -> State TestState TestResult
+runStep (Wait n) = do
+    modify (\ts -> ts { testStateTime = addUTCTime (fromIntegral n) (testStateTime ts) })
+    return Pass
+runStep (StreamerSay s) = do
+    u <- fmap (queueStreamer . testStateQueue) get
+    runStep (UserSay u s)
+runStep (UserSay u s) = do
+    time <- fmap testStateTime get
+    q <- fmap testStateQueue get
+    let (q', msg) = handleTimestamped u time (parseCommand u s) q
+    modify (\ts -> ts { testStateQueue = q', testStateLastMessage = msg })
+    return Pass
+runStep (BotSay s) = do
+    lastMsg <- fmap testStateLastMessage get
+    if lastMsg == s then return Pass else return (Fail $ "Expected " ++ show s ++ " but was " ++ show lastMsg)
